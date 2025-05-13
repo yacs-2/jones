67426c66331cc9df610dbc3c8b152b2d4be20863
yacs-2 <chromebook2@youngsvilleacademy.org>
2025-05-13 15:26:03 -0400

    Create ttt.cs

---
+++ wdsub/ttt.cs
@@ -0,0 +1,40 @@
+body {
+  margin: 0;
+  padding: 0;
+}
+.square {
+  float: left;
+  border: solid black 3px;
+  width: 32.4vw;
+  font-size: 400%;
+  text-align: center;
+  padding-top: 5%;
+  padding-bottom: 5%;
+}
+#winner{
+  display:none;
+  position: fixed;
+  top: 0px;
+  left:0px;
+  right:0px;
+  bottom: 0px;
+  text-align:center;
+  border: solid black 3px;
+  background-color:rgba(0,0,0,.5);
+  color:red;
+  font-size:200%;
+  padding: 2em;
+}
+
+#winner div {
+  margin: auto;
+  top: 100px;
+  left:25%;
+  right:25%;
+  text-align:center;
+  border: solid black 3px;
+  background-color:black;
+  color:red;
+  font-size:200%;
+  padding: 2em;
+}
